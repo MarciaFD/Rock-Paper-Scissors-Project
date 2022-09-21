a3920223574767a494c02b6804a88f576bbe780f
MarciaFD <marciadublin2015@gmail.com>
2022-09-21 17:53:39 +0000

    Add convertCase function

--- app.js
+++ app.js
@@ -8,43 +8,52 @@ const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
 
+
+/* Core Computer function*/
+
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const randomNumber = Math.floor(Math.random()*3)
     return choices[randomNumber];    
 }
 
+/*Convertcase */
+
+function convertCase(anythingIwant) {
+    if(anythingIwant ==="paper") return "Paper";
+    if(anythingIwant ==="scissors") return "Scissors";
+    return "Rock";
+}
+
+/* Core Game */
+
  function game(userChoice) {
     const computerChoice = getComputerChoice();
+    console.log ("Game function:user choice is="+userChoice);
+    console.log("Game function: computer choiceis="+computerChoice);
+
     switch(userChoice + computerChoice) {
+        case "scissorpaper":
         case "rockscissors":
         case "paperrock":
-        case "scissorspaper":
-            win();
-            break;
+        win(userChoice,computerChoice);
+        console.log("user wins");
+        
+        break;
        case "rockpaper":
        case "paperscissors":
        case "scissorsrock":
+        console.log("user losescomputer wins");
 
             break;
        case "rockrock":
        case "paperpaper":
        case "scissorscissors":
             console.log("Its a draw");
-            break;
-
-
-        
-        
-
-        
-        
-
-    }
-    
-
-
-
+            break;  
+             
+  }
+   
 
 function main() {
     rock_div.addEventListener("click", function() {
@@ -61,4 +70,3 @@ function main() {
 }
 
 main();
-
